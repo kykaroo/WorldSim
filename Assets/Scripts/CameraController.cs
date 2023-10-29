a5c5eac372f0d56e2da6abab1e8334a5f50ed100
kykaroo <boris_nemcev@mail.ru>
2023-10-29 15:29:23 +0500

    Добавлено управление камерой

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using Zenject;
+
+public class CameraController : ITickable
+{
+    private readonly Camera _camera;
+    
+    private Vector3 _lastMousePosition;
+    private float _sensitivity = 10f;
+    private bool _drag;
+
+    [Inject]
+    public CameraController(Camera camera)
+    {
+        _camera = camera;
+    }
+
+
+    public void Tick()
+    {
+        MoveCamera();
+        CameraZoom();
+    }
+
+    private void CameraZoom()
+    {
+        switch (Input.mouseScrollDelta.y)
+        {
+            case > 0:
+                _camera.transform.position += Vector3.forward * 50;
+                break;
+            case < 0:
+                _camera.transform.position -= Vector3.forward * 50;
+                break;
+        }
+    }
+
+    private void MoveCamera()
+    {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            var mouseDelta = _lastMousePosition - Input.mousePosition;
+
+            _camera.transform.position += new Vector3(mouseDelta.x, mouseDelta.y, 0);
+
+            _lastMousePosition = Input.mousePosition;
+        }
+
+        _lastMousePosition = Input.mousePosition;
+    }
+}
--- Assets/Scripts/DependencyInjection/MapGeneratorInstaller.cs
+++ Assets/Scripts/DependencyInjection/MapGeneratorInstaller.cs
@@ -9,12 +9,14 @@ namespace DependencyInjection
         [SerializeField] private GenerationConfig generationConfig;
         [SerializeField] private new Renderer renderer;
         [SerializeField] private GeneratorUi generatorUi;
+        [SerializeField] private Camera camera;
         public override void InstallBindings()
         {
             Container.BindInterfacesAndSelfTo<MapGenerator.MapGenerator>().AsSingle().NonLazy();
             Container.BindInterfacesAndSelfTo<Noise>().AsSingle().NonLazy();
             Container.BindInterfacesAndSelfTo<TextureGenerator>().AsSingle().NonLazy();
             Container.BindInterfacesAndSelfTo<MapDisplay>().AsSingle().WithArguments(renderer).NonLazy();
+            Container.BindInterfacesAndSelfTo<CameraController>().AsSingle().WithArguments(camera).NonLazy();
             
             Container.BindInterfacesAndSelfTo<GenerationConfig>().FromInstance(generationConfig).AsSingle();
             Container.BindInterfacesAndSelfTo<GeneratorUi>().FromInstance(generatorUi).AsSingle();
